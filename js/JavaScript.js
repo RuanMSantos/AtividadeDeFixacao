c6c4ef2872519a3ba866c2495379ac82ea97e3f8
RuanMSantos <ruancauancauan@gmail.com>
2024-10-11 02:07:37 -0300

    Finaliza exercício

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,12 +1,48 @@
 function Cadastrar(){
     let aluno = [
         {
-            "matricula": document.getElementById("matricula").value,
-            "nome": document.getElementById("nome").value,
-            "data de nascimento": document.getElementById("data").value,
-            "curso": document.getElementById("curso").value,
-            "sexo": document.getElementById("sexo").value
+            "matricula": 14433,
+            "nome": "Breno Vaz Borges",
+            "data nascimento": "12/06/2005",
+            "curso": "Informática",
+            "genêro":"masculino"
+        },
+        {
+            "matricula": 14466,
+            "nome": "Gabriel Duarte",
+            "data nascimento": "08/10/2007",
+            "curso": "Informática",
+            "genêro":"masculino"
+        },
+        {
+            "matricula": 12335,
+            "nome": "Ruan Cauan",
+            "data nascimento": "08/01/2005",
+            "curso": "Informática",
+            "genêro":"masculino"
+        },
+        {
+            "matricula": 13454,
+            "nome": "Nicholas Pietro",
+            "data nascimento": "07/11/2006",
+            "curso": "Informática",
+            "genêro":"masculino"
+        },
+        {
+            "matricula": 17253,
+            "nome": "Zé",
+            "data nascimento": "09/02/1993",
+            "curso": "Informática",
+            "genêro":"masculino"
         }
     ]
     console.log(aluno);
+    
+    let filter_genero = aluno.filter(a => a.genêro == "feminino");
+    let filter_genero_curso = aluno.filter(a => a.genêro == "masculino" && a.curso == "Informática");
+    let filter_matricula = aluno.filter(a => a.matricula  > 100);
+
+    console.log(filter_genero);
+    console.log(filter_genero_curso);
+    console.log(filter_matricula);
 }
